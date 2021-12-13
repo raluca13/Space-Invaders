9bfc9c3f89b76d3f75308cd2f8845a5175dcc342
Lara <Lara.unimail@gmail.com>
2021-12-13 21:28:38 +0200

    ADD: Player Death because of Enemy

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    public AudioSource audio;
+    public AudioClip destroySFX;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,21 @@ public class PlayerBehaviour : MonoBehaviour
     {
         
     }
-   
- 
-    
+
+    // A function automatically triggerred when another game object with Collider2D component
+    // Enters the Collider2D boundaries on this game object
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Check the tag on the other game object. If it's the projectile's tag,
+        //  destroy both this game object and the projectile
+        if (otherCollider.tag == "EnemyProjectile")
+        {
+            //audio.PlayOneShot(destroySFX);
+            Destroy(gameObject);
+
+            // Get the game object, as a whole, that's attached to the Collider2D component
+            Destroy(otherCollider.gameObject);
+        }
+    }
+
 }
