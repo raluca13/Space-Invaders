76082111828608c33ed15e94dcc2a9b6980a36e2
Lara <Lara.unimail@gmail.com>
2021-12-14 03:39:03 +0200

    ADD: Start Menu UI

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -6,20 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    public GameObject creditScreen; 
 
     public void GetToMenuButton()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenCredit()
+    {
+        creditScreen.SetActive(true);
+    }
+
+    public void CloseCredit()
+    {
+        creditScreen.SetActive(false);
     }
 }
